af5e0b57eaff293918846e82eaf6527c7c327580
Shaker khati <0220270.cst@rub.edu.bt>
2025-02-27 14:58:47 +0530

    my second commit

---
+++ CSF.practical.py
@@ -0,0 +1,4 @@
+studentName = "Jigme Zangmo"
+studentNumber = "02190102"
+
+print("My name is " + studentName + " and my student number is " + studentNumber+"and i am the most beautiful lady in this class . thank you")
