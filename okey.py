800a87a87f88504248097f2822a9f9a1dcc0f388
Shaker khati <0220270.cst@rub.edu.bt>
2025-02-27 14:52:14 +0530

    My first commit

---
+++ okey.py
@@ -0,0 +1,1 @@
+print("shaker khati")
---
+++ pw 1.py
@@ -0,0 +1,21 @@
+print("Hello!!!!")
+print("This is my first script!")
+
+pi = 3.14
+pi2 = int(pi)
+print(pi)
+print(pi2)
+
+pi3 = "3.14"
+print(type(pi3))
+pi4 = float(pi3)
+print(type(pi4))
+
+print(0<1)
+print(1>0)
+bool(0)
+bool(1)
+bool("Hello")
+
+x = None
+print(x)
