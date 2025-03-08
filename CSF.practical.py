2f722bb8608f78018e666563451c904b82e32858
Shaker khati <0220270.cst@rub.edu.bt>
2025-03-09 00:12:51 +0530

    my intial commit

--- CSF.practical.py
+++ CSF.practical.py
@@ -2,4 +2,4 @@ studentName = "Hritik Rioshan"
 studentNumber = "17973938"
 
 
-print("My name is " + studentName + " and my student number is " + studentNumber+"and i am the most handsome man in the world . thank you" + "Collge of science and technolgy is the best collge in bhutan")
+print("My name is " + studentName + " and my student number is ")
