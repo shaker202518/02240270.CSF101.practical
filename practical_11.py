987aa05b21dedf1cd9635376b6878b10aceedca7
Shaker Khati <02240270.cst@rub.edu.bt>
2025-05-02 23:30:19 +0530

    my prac 11 commit

--- practical_11.py
+++ practical_11.py
@@ -1,3 +1,6 @@
+from pyclbr import Class
+
+
 class Person:
   def __init__(self, name, age):
     self.name = name
@@ -5,7 +8,7 @@ class Person:
 p1 = Person("Shaker", 20)
 print(p1) 
 
-# Implementation of instnace method
+# Implementation of instnace method with stter method
 class Person:
   def __int__(self, name, age):
     self.name = name
@@ -13,4 +16,30 @@ class Person:
     
   def hello(self):
     print("Hello my name is" + self.name + "my age is " + str(self.age))
+  @property
+  def age (self):
+    return self.__age
+  
+  @age.setter
+  def age(self, newage):
+    self._age = newage
+    
+  def __str__(self):
+    return f"{self.name}({self.age})"
+  
   p1 = Person("shaker", 20)
+  print(p1)
+  
+#Implentation of Subclass 
+class Dancer():
+  def __init__(self, name, age, genre):
+    super().__init__(name, age)
+    self.genre = genre
+    
+  def dance(self):
+    print("my name is " + self.name + " my favourite dance is" + self.genre)
+    
+  def __str__(self):
+    return f"{self.name}({self.age}) does {self.genre}"
+d1 = Dancer("Hritik Roshan", 45, "Moon walk" )
+print(d1)
