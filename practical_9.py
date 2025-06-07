cbab1e6e6270c0829a93961e0bfee0edfc8a5875
Shaker Khati <02240270.cst@rub.edu.bt>
2025-06-07 18:13:35 +0300

    My prac 9

---
+++ practical_9.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+# Test the append method
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+
+class LinkedList:
+    def __init__ (self):
+        self.head = None
+         
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        print(" -> ".join(map(str, elements)))
+
+# Test the display method
+ll.display()  # Output: 1 -> 2 -> 3
